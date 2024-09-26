3036abf3b39ad0e9a68c5e616d168b85a4b04e34
user <user@email.com>
2024-09-26 13:08:23 +0900

    엔터없는 출력

--- src/day01/ExClass01.java
+++ src/day01/ExClass01.java
@@ -3,5 +3,9 @@ public class ExClass01 {
 	public static void main(String[] args) {
 		//System.out.println(100);
 		System.out.println("안녕하세요");
+		System.out.print("1.안녕");
+		System.out.print("1.안녕");
+		System.out.print("1.안녕");
+	
 	}
 }
